9db3d8ea931c9e8332fa159b4b98845f7b7ae438
Rafal Sroka <rafal.sroka@srf.com>
2025-09-11 16:51:35 +0200

    Simplify code, add repository

--- furka/lib/main.dart
+++ furka/lib/main.dart
@@ -13,22 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 238, 0)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 238, 0),
+          primary: const Color.fromARGB(255, 255, 238, 0),
+        ),
       ),
       home: const HomePage(title: 'Furka'),
     );
@@ -38,25 +26,27 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
+class Place {
+  String name;
+  double latitude;
+  double longitude;
+  int altitude;
+
+  Place(this.name, this.latitude, this.longitude, this.altitude) {}
+}
+
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  PlacesRepository repository = PlacesRepository();
+
+  void _addPlace() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -69,54 +59,71 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ListView.separated(
+          itemCount: repository.places.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(repository.places[index].name),
+              subtitle: Text('${repository.places[index].altitude} m'),
+              onTap: () {
+                // Go to the next screen with Navigator.push
+              },
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _addPlace,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+}
+
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+/// A ListItem that contains data to display a heading.
+class PlaceItem implements ListItem {
+  final String title;
+  final String subtitle;
+
+  PlaceItem(this.title, this.subtitle);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(title, style: Theme.of(context).textTheme.headlineSmall);
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(subtitle);
+}
+
+class PlacesRepository {
+  var places = [
+    Place("Galenstock", 46.61263, 8.41712, 3586),
+    Place("Klein Furkahorn", 46.58573, 8.40629, 3026),
+    Place("Gross Furkahorn", 46.59661, 8.41030, 3169),
+    Place("Sidelenhorn", 46.60085, 8.40974, 3215),
+    Place("Chli Bielenhorn", 46.59891, 8.43489, 2940),
+    Place("Gross Bielenhorn", 46.60360, 8.42859, 3210),
+    Place("Gletschhorn", 46.61803, 8.44086, 3305),
+    Place("Winterstock", 46.61778, 8.45467, 3203),
+    Place("Tiefenstock", 46.64000, 8.42000, 3515),
+  ];
 }

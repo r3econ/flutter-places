889159811bfe9683f9089cb401775d62659c8708
Rafal Sroka <rafal.sroka@srf.com>
2025-09-11 17:09:13 +0200

    Rename the app

--- furka/lib/main.dart
+++ furka/lib/main.dart
@@ -3,11 +3,11 @@ import '/repositories/places_repository.dart';
 import 'configuration/app_configuration.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   // This widget is the root of your application.
   @override

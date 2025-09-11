d6933e32bbb41926a8917e865f518ce305bb443f
Rafal Sroka <rafal.sroka@srf.com>
2025-09-11 17:31:09 +0200

    Tweak appearance

--- furka/lib/main.dart
+++ furka/lib/main.dart
@@ -50,8 +50,9 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title),
-      ),
+        title: Text(widget.title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold))),
       body: Center(
         child: ListView.separated(
           itemCount: repository.places.length,

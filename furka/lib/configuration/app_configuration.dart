b11c3be35d2af5c2585e7b7ebf5d76ab279520aa
Rafal Sroka <rafal.sroka@srf.com>
2025-09-11 18:55:31 +0200

    Make the app white

--- furka/lib/configuration/app_configuration.dart
+++ furka/lib/configuration/app_configuration.dart
@@ -4,8 +4,8 @@ class AppConfiguration {
   static const String appName = "Furka";
   static final ThemeData theme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color.fromARGB(255, 245, 216, 64),
-      primary: const Color.fromARGB(255, 245, 216, 64),
+      seedColor: const Color.fromARGB(255, 255, 255, 255),
+      primary: const Color.fromARGB(255, 255, 255, 255),
     ),
   );
 }

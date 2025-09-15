f16ff2b1ec624edb1464740739e0cf3e1e2581c4
Rafal Sroka <rafal.sroka@srf.com>
2025-09-15 11:27:56 +0200

    Use cross platform tab bar

--- furka/lib/main.dart
+++ furka/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:furka/pages/places_map_page.dart';
 import 'configuration/app_configuration.dart';
 import 'pages/places_list_page.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 void main() => runApp(const App());
 
@@ -13,7 +14,8 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: AppConfiguration.appName,
       theme: AppConfiguration.theme,
-      home: MainTabBar());
+      home: MainTabBar(),
+    );
   }
 }
 
@@ -25,36 +27,36 @@ class MainTabBar extends StatefulWidget {
 }
 
 class _MainTabBarState extends State<MainTabBar> {
-  int currentPageIndex = 0;
+  int _selectedPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: theme.colorScheme.onSecondary,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.list_outlined),
-            label: 'Places',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.map_outlined),
-            label: 'Map',
-          ),
-        ],
-      ),
-      body: <Widget>[
+    return PlatformScaffold(
+      body: [
         /// Home page
         PlacesListPage(title: AppConfiguration.appName),
         PlacesMapPage(),
-      ][currentPageIndex],
+      ][_selectedPageIndex],
+      bottomNavBar: PlatformNavBar(
+        currentIndex: _selectedPageIndex,
+        cupertino: (context, platform) => CupertinoTabBarData(
+          backgroundColor: theme.colorScheme.primary,
+          activeColor: theme.colorScheme.inverseSurface,
+        ),
+        itemChanged: (index) => setState(() {
+          _selectedPageIndex = index;
+        }),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_outlined),
+            label: 'Places',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.map_outlined), label: 'Map'),
+        ],
+      ),
+      iosContentPadding: false,
+      iosContentBottomPadding: false,
     );
   }
 }
